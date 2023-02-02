5decdaaceb4a7b052d4d2f90d0f758c42e5ca25a
mligerhe <martaligerohernandez@gmail.com>
2023-02-02 16:50:16 +0100

    update readme

---
+++ README.md
@@ -0,0 +1,8 @@
+To run feature extraction do 
+
+´´´
+python imagenet.py /input/path/* --outdir /output/path/vit
+python radimagenet.py /input/path/* --outdir /output/path/vit
+python vit.py /input/path/* --outdir /output/path/vit
+python swint.py /input/path/* --outdir /output/path/vit
+´´´ 
--- extractfeats.sh
+++ extractfeats.sh
@@ -1,5 +1,5 @@
 for feats in imagenet radimagenet vit swint
 do
 	echo "Running target: ${target}"
-	python ${feats}.py /nfs/rnas/mligero/Github/GBM/data/brain_slice/T1/* --outdir /nfs/rnas/mligero/Github/FeatureExtraction_RAD/${feats}
+	python ${feats}.py /input/path/* --outdir /output/path/${feats}
 done
